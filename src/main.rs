5cbf3a2f82969914694760c0213d4cc118c91928
marnixah <marnix@hagehoeve.com>
2021-12-02 21:30:57 +0100

    feat: popping and running jobs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+mod handlers;
+mod jobs;
+pub use handlers::yt_dlp;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        pop_and_run_job();
+    }
+}
+
+fn pop_and_run_job() {
+    let mut job = jobs::pop_job();
+    println!("Running job for: {:?}", job.url);
+    job.update_status("running".to_string());
+
+    let handler = match job.handler.as_ref() {
+        "yt-dlp" => yt_dlp::handle,
+        _ => panic!("Handler not found"),
+    };
+
+    let tmp_file = handler(&job.url);
+    fs::create_dir_all(job.save_folder()).unwrap();
+    let tmp_file_path = tmp_file.unwrap();
+    fs::copy(tmp_file_path, job.save_location.clone()).unwrap();
+    fs::remove_file(tmp_file_path).unwrap();
+
+    job.update_status("finished".to_string());
+    println!("Finished job for: {:?}", job.url);
 }
