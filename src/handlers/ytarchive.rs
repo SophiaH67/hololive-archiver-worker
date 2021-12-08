6fb7989f5d052f3624c5dd4f445dca2f5c93e165
marnixah <marnix@hagehoeve.com>
2021-12-08 11:48:09 +0100

    fix: haystack contains checks in ytarchive

--- src/handlers/ytarchive.rs
+++ src/handlers/ytarchive.rs
@@ -18,23 +18,24 @@ pub fn handle(url: &str) -> Result<&'static str, String> {
         Ok("/tmp/ytarchive.mkv")
     } else {
         let stderr = String::from_utf8_lossy(&output.stderr);
-
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let output = format!("{}\n{}", stderr, stdout);
         // Using regex, check if "no space left on device" is anywhere in the stderr ignoring case
         let space_regex = Regex::new(r"no space left on device").unwrap();
-        if space_regex.is_match(&stderr) {
+        if space_regex.is_match(&output) {
             return Err(String::from("No space left on device"));
         }
         
         // Try yt-dlp if yt-archive error contains "youtube-dl"
         let ytdlp_regex = Regex::new(r"youtube-dl").unwrap();
-        if ytdlp_regex.is_match(&stderr) {
+        if ytdlp_regex.is_match(&output) {
             println!("ytarchive recommends using youtube-dl, switching to yt-dlp");
             return yt_dlp::handle(url);
         }
 
         // Return an error with either stdout if length of stderr is 0
         return if stderr.len() == 0 {
-            Err(String::from_utf8_lossy(&output.stdout).into_owned())
+            Err(stdout.into_owned())
         } else {
             Err(stderr.into_owned())
         };
